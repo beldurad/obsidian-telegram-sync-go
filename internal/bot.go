f8b93fdf622a06e167c01c23525bcbd0fd4a8508
beldurad <bulatkash1788@gmail.com>
2026-06-04 10:00:47 +0300

    bot

---
+++ internal/bot.go
@@ -0,0 +1,47 @@
+package app
+
+type UserMessage struct {
+	ChatID int
+	From   string
+	Text   string
+}
+
+type BotResponse struct {
+	ChatID int    `json:"chat_id"`
+	Text   string `json:"text"`
+	Err    error
+}
+
+type MessageHandler interface {
+	Handle(UserMessage) BotResponse
+	Supports(UserMessage) bool
+}
+
+type Bot struct {
+	messages  <-chan UserMessage
+	handlers  []MessageHandler
+	responses chan BotResponse
+}
+
+func (b *Bot) ListenToMessages() {
+	for m := range b.messages {
+		go b.Handle(m)
+	}
+}
+
+func (b *Bot) Responses() <-chan BotResponse {
+	return b.responses
+}
+
+func (b *Bot) Handle(m UserMessage) {
+	for _, h := range b.handlers {
+		if h.Supports(m) {
+			b.responses <- h.Handle(m)
+			return
+		}
+	}
+	b.responses <- BotResponse{
+		ChatID: m.ChatID,
+		Text:   "I can't understand you",
+	}
+}
